5928f5098801e3c5516e76ec0006726053c233f1
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-19 06:34:29 -0500

    Update App.js
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import KnowledgeBase from './components/KnowledgeBase/KnowledgeBase';
+import ArticleDetail from './components/KnowledgeBase/ArticleDetail';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/kb" component={KnowledgeBase} />
+        <Route path="/kb/:id" component={ArticleDetail} />
+        {/* Add other routes here */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+import React from 'react';
 import Dashboard from './components/Dashboard';
 
 function App() {
