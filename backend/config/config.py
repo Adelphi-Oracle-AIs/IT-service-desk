1d27b0ed4256aff208df4dc87d631d739a396f15
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-19 06:20:57 -0500

    Update config.py
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,4 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    ARTICLES_PER_PAGE = 10
+import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
