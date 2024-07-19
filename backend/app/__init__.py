17f1ac41c70741c5c0de3ca305abb30077099d69
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-19 06:22:46 -0500

    Update __init__.py
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from elasticsearch import Elasticsearch
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+es = Elasticsearch([Config.ELASTICSEARCH_URL])
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    from app.knowledge_base import bp as kb_bp
+    app.register_blueprint(kb_bp, url_prefix='/api/kb')
+
+    return app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from config.config import Config
 
