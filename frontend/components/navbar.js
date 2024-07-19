6c87c53a606c761967a8dfb30629ba77d0067d65
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-19 07:18:51 -0500

    Create navbar.js
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

---
+++ frontend/components/navbar.js
@@ -0,0 +1,22 @@
+// src/components/Navbar.js
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          {/* INPUT NEEDED: Choose your app name */}
+          IT Optimization SaaS
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Dashboard</Button>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+        <Button color="inherit" component={Link} to="/register">Register</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
