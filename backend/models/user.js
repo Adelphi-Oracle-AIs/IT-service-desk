fc74da63ba1b810acf3f965e6c7eeb765ca153d2
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-19 09:19:02 -0500

    Create user.js
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

---
+++ backend/models/user.js
@@ -0,0 +1,40 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'manager', 'developer'],
+    default: 'developer'
+  }
+}, { timestamps: true });
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
