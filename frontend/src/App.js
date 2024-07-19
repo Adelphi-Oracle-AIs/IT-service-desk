51ec489e3736ea46fffbdfeb32574d84951eb6d9
EdenEven <administratornikiruss@edenstreeacademy.com>
2024-07-18 23:58:42 -0500

    Create App.js
    Signed-off-by: EdenEven <administratornikiruss@edenstreeacademy.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>IT Service Desk</h1>
+      </header>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
